90cd7cff5f4769e8c403558523409a049e8e5a17
Darrylwin <logossoudarryl20@gmail.com>
2026-04-08 21:05:16 +0000

    add: implement user registration logic in UserServiceImpl; check for existing users and encode passwords

--- src/main/java/com/darcode/curalink/service/impl/UserServiceImpl.java
+++ src/main/java/com/darcode/curalink/service/impl/UserServiceImpl.java
@@ -3,11 +3,39 @@ package com.darcode.curalink.service.impl;
 import com.darcode.curalink.dto.RegistrationRequestDto;
 import com.darcode.curalink.dto.RegistrationResponseDto;
 import com.darcode.curalink.model.User;
+import com.darcode.curalink.repository.UserRepository;
 import com.darcode.curalink.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
     @Override
     public RegistrationResponseDto register(RegistrationRequestDto registrationRequestDto) {
-        User userExist =
+        User userExist = userRepository.findByEmail(registrationRequestDto.email()).orElse(null);
+
+        if (userExist != null) {
+            return new RegistrationResponseDto("User already exists");
+        }
+
+        User user = new User();
+        user.setFirstName(registrationRequestDto.firstName());
+        user.setEmail(registrationRequestDto.email());
+        user.setPassword(passwordEncoder.encode(registrationRequestDto.password()));
+
+        userRepository.save(user);
+
+        return new RegistrationResponseDto("Registration Successfull");
     }
 }
