8e4de068838106fec71904092cc619701ffb3b9e
Darrylwin <logossoudarryl20@gmail.com>
2026-04-08 20:50:33 +0000

    add: include JWT dependencies in pom.xml; add method to find User by email in UserRepository

--- src/main/java/com/darcode/curalink/repository/UserRepository.java
+++ src/main/java/com/darcode/curalink/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.darcode.curalink.repository;
 
 import com.darcode.curalink.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
 }
