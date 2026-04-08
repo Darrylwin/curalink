c603b5517509eb021ddc81bb1dd23122ba86c949
Darrylwin <logossoudarryl20@gmail.com>
2026-04-08 21:29:53 +0000

    add: implement UserController for user registration; create UserDetailsImpl for user authentication

---
+++ src/main/java/com/darcode/curalink/user/UserController.java
@@ -0,0 +1,30 @@
+package com.darcode.curalink.user;
+
+import com.darcode.curalink.dto.ApiResponse;
+import com.darcode.curalink.dto.RegistrationRequestDto;
+import com.darcode.curalink.dto.RegistrationResponseDto;
+import com.darcode.curalink.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<RegistrationResponseDto>> register(@RequestBody RegistrationRequestDto registrationRequestDto) {
+        RegistrationResponseDto response = userService.register(registrationRequestDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(response, response.message()));
+    }
+}
