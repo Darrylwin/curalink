c89ebcbefb7b3a36151533393541f34c36e2fc39
Darrylwin <logossoudarryl20@gmail.com>
2026-04-10 14:17:49 +0000

    add role enumeration to User and update authorities in UserDetailsImpl

--- src/main/java/com/darcode/curalink/model/User.java
+++ src/main/java/com/darcode/curalink/model/User.java
@@ -26,6 +26,7 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role = Role.PATIENT;
 
     @Column(nullable = false)
